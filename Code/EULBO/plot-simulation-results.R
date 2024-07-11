fafd7360a99800600f5bc9deb19c5d3e93bd71ac
hotz1 <johotz37@gmail.com>
2024-07-11 14:48:42 -0700

    Making plots and combined datasets

--- Code/EULBO/plot-simulation-results.R
+++ Code/EULBO/plot-simulation-results.R
@@ -1,18 +1,25 @@
 library(here)
 library(tidyverse)
 
-sims <- read_csv("./Code/EULBO/Sim-Results/Separate_Simulation_Results_Full.csv")
+# Read data
+# sims <- read_csv(here("Code/EULBO/Sim-Results/Combined/___.csv"))
+
+# Make Simulation no. categorical
 sims <- sims %>%
   mutate(Simulation = as.factor(Simulation))
 
+# Create plot
 sims %>%
   ggplot(aes(x = Epoch, y = trueBest, group = Simulation, colour = Simulation)) +
   geom_line() +
   facet_wrap(~Actions, labeller = as_labeller(function(t) paste(t, "Actions"))) +
   theme_bw() +
-  labs(title = "20 Simulations of Separate Optimization for (S, x)") +
-  theme(legend.position = "none", plot.title = element_text(hjust = 0.5)) +
+  labs(title = "10 Simulations of Separate Optimization for (S, x)",
+       subtitle = "N = 200") +
+  theme(legend.position = "none", plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5)) +
   geom_hline(yintercept = 3.32237, color = "red", linetype = "dashed")
 
-ggsave("./Code/EULBO/Sim-Results/Separate_Simulation_Plots.png", device = "png",
+# Save plot
+ggsave(here("Code/EULBO/Sim-Results/Plots/N200_Plots.png"), device = "png",
        width = 10, height = 10, units = "in")
