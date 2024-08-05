6f5dd89f32ce39c36bf4d2534473f5048a869c16
Joey Hotz <johotz37@gmail.com>
2024-08-05 11:24:59 -0700

    Updated batch simulations script

--- Code/EULBO/bash/batch_elbo_fixed_sims.sh
+++ Code/EULBO/bash/batch_elbo_fixed_sims.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
+#SBATCH --output="/home/joeyhotz/scratch/BayesOpt-Research/slurm-logs/slurm-%j.out"
 
 # Script to run one single job 
 cd ~/scratch/BayesOpt-Research
-JOB_SCRIPT="Code/EULBO/shells/run_elbo_fixed_sims.sh"
+JOB_SCRIPT="Code/EULBO/bash/run_elbo_fixed_sims.sh"
 
 # Submit the first job
-PREV_JOB_ID=$(sbatch --export=ALL "$JOB_SCRIPT" | awk '{print $4}')
+PREV_JOB_ID=$(sbatch --gpus-per-node=$JOBGPUS --mem=$JOBMEM --time=$JOBTIME --export=ALL "$JOB_SCRIPT" | awk '{print $4}')
 echo "Submitted job 1 with Job ID: $PREV_JOB_ID"
 
 # Loop to submit the remaining jobs with dependencies
 for (( i=2; i<=NUM_JOBS; i++ ))
 do
   # Submit the job with dependency on the previous job
-  JOB_ID=$(sbatch --export=ALL --dependency=after:$PREV_JOB_ID+$DELAY "$JOB_SCRIPT" | awk '{print $4}')
+  JOB_ID=$(sbatch --gpus-per-node=$JOBGPUS --mem=$JOBMEM --time=$JOBTIME --export=ALL --dependency=after:$PREV_JOB_ID+$DELAY "$JOB_SCRIPT" | awk '{print $4}')
   
   # Print the job ID for tracking
   echo "Submitted job $i with Job ID: $JOB_ID"
