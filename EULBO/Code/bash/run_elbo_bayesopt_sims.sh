157e623c4f798bf58daf9f82f2a99d267808946f
hotz1 <johotz37@gmail.com>
2024-08-19 21:10:07 -0700

    Fixing filepaths in shell scripts

--- EULBO/Code/bash/run_elbo_bayesopt_sims.sh
+++ EULBO/Code/bash/run_elbo_bayesopt_sims.sh
@@ -7,7 +7,7 @@ module load python/3.10
 source ~/envs/research/bin/activate
 
 cd ~/scratch/BayesOpt-Research
-cmd="python Code/EULBO/elbo_simulate_fire.py ELBO_BayesOpt --N_init=${N_init} --n_simulations=${n_simulations} --n_epochs=${n_epochs}"
+cmd="python EULBO/Code/elbo_simulate_fire.py ELBO_BayesOpt --N_init=${N_init} --n_simulations=${n_simulations} --n_epochs=${n_epochs}"
 echo $cmd
 echo ""
 eval $cmd
