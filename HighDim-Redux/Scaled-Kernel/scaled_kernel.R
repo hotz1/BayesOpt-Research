1cc6278eee632589999cbf5414ef269f1958b877
hotz1 <johotz37@gmail.com>
2024-11-04 15:58:19 -0800

    Adding blank R file

---
+++ HighDim-Redux/Scaled-Kernel/scaled_kernel.R
@@ -0,0 +1,7 @@
+# Code for a Bayesian optimization framework when the true objective function
+# is a scalar multiple of the covariance kernel
+
+rbf_kernel <- function(X1, X2, ls, os){
+  k = 0
+  return(k)
+}
